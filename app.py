22f8a7ba35a3c4299043111cac652e0179977d2d
Bitopan Das <bitopan21mbb@gmail.com>
2025-04-18 19:34:18 +0000

    changed requirements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from agno.agent import Agent
-from agno.models.google import Gemini
-from agno.tools.duckduckgo import DuckDuckGoTools
+from phi.agent import Agent
+from phi.model.google import Gemini
+from phi.tools.duckduckgo import DuckDuckGo
 from google.generativeai import upload_file,get_file
 import google.generativeai as genai
 import time
@@ -29,7 +29,7 @@ def initialize_agent():
     return Agent(
         name="Video AI Summarizer",
         model=Gemini(id="gemini-2.0-flash-exp"),
-        tools=[DuckDuckGoTools()],
+        tools=[DuckDuckGo()],
         markdown=True,
     )
 
